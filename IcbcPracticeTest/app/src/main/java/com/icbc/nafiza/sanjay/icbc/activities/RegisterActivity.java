bf65abb21252f9be49f016ec080caf40326feef0
nafizamann <36461003+nafizamann@users.noreply.github.com>
2018-11-17 23:52:56 -0800

    Change layout design in all xml files.

--- IcbcPracticeTest/app/src/main/java/com/icbc/nafiza/sanjay/icbc/activities/RegisterActivity.java
+++ IcbcPracticeTest/app/src/main/java/com/icbc/nafiza/sanjay/icbc/activities/RegisterActivity.java
@@ -2,14 +2,29 @@ package com.icbc.nafiza.sanjay.icbc.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.icbc.nafiza.sanjay.icbc.R;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    Button btnRegister;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        btnRegister=(Button)findViewById(R.id.btnRegister);
+        addListener();
+
+    }
+
+    public void addListener(){
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
