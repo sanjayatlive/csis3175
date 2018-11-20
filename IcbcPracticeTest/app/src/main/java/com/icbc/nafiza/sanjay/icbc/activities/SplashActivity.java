379c4ab91eafcf192802f9496e077e8c126610ae
nafizamann <36461003+nafizamann@users.noreply.github.com>
2018-11-19 19:08:13 -0800

    Make Splash Screen Time 2 seconds.

--- IcbcPracticeTest/app/src/main/java/com/icbc/nafiza/sanjay/icbc/activities/SplashActivity.java
+++ IcbcPracticeTest/app/src/main/java/com/icbc/nafiza/sanjay/icbc/activities/SplashActivity.java
@@ -14,7 +14,7 @@ import com.icbc.nafiza.sanjay.icbc.util.DBHelper;
 
 public class SplashActivity extends AppCompatActivity {
     DBHelper dbHelper;
-    private static int SPLASH_TIME_OUT = 3000;
+    private static int SPLASH_TIME_OUT = 2000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
